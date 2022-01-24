a007424da5ab5a4bd31879904b33b6f8fdbfe4e8
Aylín Alvarez <62406442+Chains99@users.noreply.github.com>
2022-01-24 02:05:01 -0500

    Update app.py

--- code/app.py
+++ code/app.py
@@ -2,9 +2,14 @@ from glob import escape
 from pydoc import doc
 from flask import Flask,render_template, request
 import time
-from program import run_program
+from program import *
+
 
 app = Flask(__name__)
+initialize()
+print('Hola')
+
+
 
 @app.route("/")
 def search():
@@ -13,10 +18,8 @@ def search():
 @app.route("/search_results")
 def search_results():
     query=request.args.get('query')
-
     inicio=time.time()
     final=time.time()
+    print(query)
     docs=run_program(query)
-
-
-    return render_template('search_results.html',query=query,time=final-inicio,docs=docs[:10])
+    return render_template('search_results.html',query=query,time=final-inicio)
