a26cf2ee14d01a290551dedcf1e424b9fef38003
Dri <82615542+adriAlvarenz@users.noreply.github.com>
2022-01-24 23:49:13 -0500

    kkk

--- code/app.py
+++ code/app.py
@@ -4,22 +4,27 @@ from flask import Flask,render_template, request
 import time
 from program import *
 
+<<<<<<< Updated upstream
 initialize()
 
+=======
+>>>>>>> Stashed changes
 app = Flask(__name__)
-
 
 @app.route("/")
 def search():
+    initialize()
     return render_template('index.html')
 
 @app.route("/search_results")
 def search_results():
     global docs
     query=request.args.get('query')
+
     inicio=time.time()
     docs_len=run_program(query)
     final=time.time()
+<<<<<<< Updated upstream
     len_=docs_len[0]
     docs=docs_len[1]
     min_len=min(len(docs),20)
@@ -28,3 +33,9 @@ def search_results():
 @app.route("/doc/<int:index>")
 def doc(index):
     return render_template('doc.html',doc=docs[index])
+=======
+    docs=run_program(query)
+
+
+    return render_template('search_results.html',query=query,time=final-inicio,docs=docs[:10])
+>>>>>>> Stashed changes
